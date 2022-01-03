f7ef71d0e56a3a7cef3bb0da1acbac14397a85c4
dmangames <talktodmangames@gmail.com>
2022-01-02 22:56:20 -0600

    made the draggable object come out on top

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -6,22 +6,31 @@ using UnityEngine;
 public class Draggable : MonoBehaviour
 {
     private Vector3 screenPoint;
+    private Vector3 originalPosition;
     private Vector3 offset;
+    private Vector3 zlift;
 
     private void Start()
     {
-        screenPoint = transform.position;
+        originalPosition = transform.position;
+        screenPoint = Camera.main.transform.position;
+        zlift = new Vector3(0, 0, -5);
     }
 
     private void OnMouseDown()
     {
-        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
     }
 
     private void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset + zlift;
         transform.position = curPosition;
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, originalPosition.z);
     }
 }
