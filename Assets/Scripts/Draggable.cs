5272e14fcdb92d3a1ce785b46fb6cdf6a19a2017
dmangames <talktodmangames@gmail.com>
2022-01-02 23:55:31 -0600

    fixed drag bug (trigger zones were in front of camera blocking player clicks)

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -19,12 +19,12 @@ public class Draggable : MonoBehaviour
 
     private void OnMouseDown()
     {
-        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
     private void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset + zlift;
         transform.position = curPosition;
     }
